1fee4a12c92799878b0ff00eae25c02be18aada7
Roy Mosby <ego.madking@gmail.com>
2020-12-10 21:16:26 -0500

    commit tree 9acf509496298067c774435a8457e3766617076d

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num <=1
+    return false
+  elsif num == 2
+    return true
+  else
+    divisor = 2
+    while divisor <= num/2 do
+      if num % divisor == 0
+        return false
+      end
+        divisor += 1
+    end
+    return true
+  end
+end
